a626bd14ed678fa380ef96544446d7a298785c02
ZuzaB <lacznaz@wp.pl>
2018-01-02 21:25:59 +0100

    clear css and add js code

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict';
+document.addEventListener("DOMContentLoaded", function(e) {
+  var next = document.querySelector('.wrap__arrow--right'),
+      prev = document.querySelector('.wrap__arrow--left');
+
+  next.addEventListener('click', function(e){
+
+  });
+
+  prev.addEventListener('click', function(e){
+
+  });
+
+});
