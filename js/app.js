cc706dbd801bdc4a2553a0ca29cf3cc94e6e410a
ZuzaB <lacznaz@wp.pl>
2018-01-03 18:41:24 +0100

    working on slider with articles

--- js/app.js
+++ js/app.js
@@ -1,14 +1,25 @@
 'use strict';
 document.addEventListener("DOMContentLoaded", function(e) {
   var next = document.querySelector('.wrap__arrow--right'),
-      prev = document.querySelector('.wrap__arrow--left');
+      prev = document.querySelector('.wrap__arrow--left'),
+      slidesContainer = document.querySelector('.slides'),
+      articles = document.querySelectorAll('.article'),
+      artikelsNumber = articles.length,
+      counter = 1;
 
   next.addEventListener('click', function(e){
-
+    var width = document.querySelector('.article').offsetWidth;
+    slidesContainer.style.left = '-' + (counter * width) +'px';
+    counter++;
+    if (counter >= artikelsNumber) {
+      slidesContainer.style.left = '0';
+      counter = 1;
+    }
   });
 
   prev.addEventListener('click', function(e){
 
   });
+
 
 });
