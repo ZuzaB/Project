edaeda65a6e11510510c0cf0eea5837da1ed6592
ZuzaB <lacznaz@wp.pl>
2018-01-08 19:12:06 +0100

    finshed working on articles slider

--- js/app.js
+++ js/app.js
@@ -3,23 +3,43 @@ document.addEventListener("DOMContentLoaded", function(e) {
   var next = document.querySelector('.wrap__arrow--right'),
       prev = document.querySelector('.wrap__arrow--left'),
       slidesContainer = document.querySelector('.slides'),
+      left = slidesContainer.style.left,
+      width = document.querySelector('.article').offsetWidth,
       articles = document.querySelectorAll('.article'),
       artikelsNumber = articles.length,
       counter = 1;
 
-  next.addEventListener('click', function(e){
-    var width = document.querySelector('.article').offsetWidth;
-    slidesContainer.style.left = '-' + (counter * width) +'px';
+window.addEventListener('resize', function(){
+  width = document.querySelector('.article').offsetWidth;
+});
+
+function nextArticle (elem) {
+  if (counter < artikelsNumber) {
+    elem.style.left = '-' + counter * width +'px';
+    counter ++;
+  } else if(counter = artikelsNumber) {
+    elem.style.left = '0px';
+    counter = 1;
+  }
+}
+
+function prevArticle(elem) {
+  if (counter > 1) {
+    counter = counter - 2;
+    elem.style.left = '-' + counter * width +'px';
     counter++;
-    if (counter >= artikelsNumber) {
-      slidesContainer.style.left = '0';
-      counter = 1;
-    }
+  } else if(counter = 1) {
+    counter = artikelsNumber - 1;
+    elem.style.left = '-' + counter * width +'px';
+    counter++;
+  }
+}
+
+  next.addEventListener('click', function(e){
+      nextArticle(slidesContainer);
   });
 
   prev.addEventListener('click', function(e){
-
+      prevArticle(slidesContainer);
   });
-
-
 });
